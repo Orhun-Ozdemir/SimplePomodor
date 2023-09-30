620ddbe0bac236a55fd9be6130e0dbafe326cc61
Orhun Özdemir <orozdemir@borusan.com>
2023-09-30 21:17:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simplepomodor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simplepomodor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
